96f5769969731648f7c8e889fcd833515b81cd69
mohammadhossein-asadi <mohammadhosseinasadi.p@gmail.com>
2023-08-04 11:46:27 +0330

    Add assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1 className="text-violet-700 font-bold">Hello</h1>;
+  const [darkMode, setDarkMode] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={`w-full h-full min-h[100vh] bg-white ${darkMode && "dark"}`}
+    >
+      <div className="w-full h-full min-h-[100vh] bg-white">
+        <div className="dark:bg-gradient-to-tr from-[#0235a3] via-[#030a1c] to-[#05174e]">
+          <Navbar darkMode={darkMode} isOpen={isOpen} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
