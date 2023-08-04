45b9a93a386d52934551e4b12aaf9648bcc1fdc7
mohammadhossein-asadi <mohammadhosseinasadi.p@gmail.com>
2023-08-04 13:56:54 +0330

    finish Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const toggleTheme = () => {
+    setDarkMode((prev) => !prev);
+  };
 
   return (
     <div
@@ -11,7 +19,12 @@ function App() {
     >
       <div className="w-full h-full min-h-[100vh] bg-white">
         <div className="dark:bg-gradient-to-tr from-[#0235a3] via-[#030a1c] to-[#05174e]">
-          <Navbar darkMode={darkMode} isOpen={isOpen} />
+          <Navbar
+            darkMode={darkMode}
+            isOpen={isOpen}
+            toggleMenu={toggleMenu}
+            toggleTheme={toggleTheme}
+          />
         </div>
       </div>
     </div>
