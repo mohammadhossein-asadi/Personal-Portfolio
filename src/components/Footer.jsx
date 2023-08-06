7dffaa8dac57889d996e807ba78ba7fef8ce86c4
mohammadhossein-asadi <mohammadhosseinasadi.p@gmail.com>
2023-08-06 16:40:36 +0330

    fix footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className="w-full flex flex-col md:flex-row pb-[2rem] pt-20 px-8 md:px-10 gap-10 lg:gap-20 justify-between ">
         <p className="text-lg text-white font-semibold ">Tehran</p>
 
-        <div className="flex flex-col md:flex-row text-md gap-5 md:gap-20 text-white pl-[200px]">
+        <div className="flex flex-col md:flex-row text-md gap-5 md:gap-20 text-white xss:items-center">
           <a href="#home">Home</a>
           <a href="#about">About</a>
           <a href="#projects">Projects</a>
