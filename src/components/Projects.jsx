5437dff53716d29f44d38772df18184ecdd85efc
mohammadhossein-asadi <mohammadhosseinasadi.p@gmail.com>
2023-10-03 23:29:57 +0330

    fix tools styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Project = () => {
         {tools.map((tool) => (
           <div
             key={tool.name}
-            className="flex gap-2 items-center bg-white rounded-md shadow-lg px-12 py-5 hover:animate-pulse"
+            className="flex w-60 gap-2 items-center bg-white rounded-md shadow-lg px-12 py-5 hover:animate-pulse"
           >
             <img src={tool.icon} className="w-10 h-10" />
             <p className=" text-black text-2xl font-semibold">{tool.name}</p>
